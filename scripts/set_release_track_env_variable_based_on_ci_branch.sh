516b9f31d96fc24e5e20283f4960d3db8a49062a
ObjectiveTruth <j.mendez@rogers.com>
2016-03-25 13:51:28 -0400

    Added a conditional setting of the release track based on branch

---
+++ scripts/set_release_track_env_variable_based_on_ci_branch.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+##############################################################################
+##
+##  Custom script executed by Travis-CI before deploying an apk.
+##
+##  Sets ANDROID_APK_RELEASE_TRACK based on the current Travis-CI branch
+##  master = production
+##  beta = beta
+##
+##############################################################################
+
+
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+    export ANDROID_APK_RELEASE_TRACK=prod
+elif [ "$TRAVIS_BRANCH" == "beta" ]; then
+    export ANDROID_APK_RELEASE_TRACK=beta
+else
+    echo "TRAVIS_BRANCH env variable not set"
+    exit 1
+fi
+
+echo "If build succeeds, apk will be pushed to ${ANDROID_APK_RELEASE} track"
