6782acc175cf12420af672860bac1e31d9a41e29
ObjectiveTruth <j.mendez@rogers.com>
2016-03-28 01:18:57 -0400

    Moved constants from MainActivity to its own package/class

---
+++ UoitDCLibraryBooking/src/main/java/com/objectivetruth/uoitlibrarybooking/constants/SHARED_PREFERENCES_KEYS.java
@@ -0,0 +1,13 @@
+package com.objectivetruth.uoitlibrarybooking.constants;
+
+public class SHARED_PREFERENCES_KEYS {
+    public static final String SHARED_PREF_REGISTRATIONID = "shared_pref_gcm_registration_id";
+    public static final String SHARED_PREF_REGISTRATION_VERSION = "shared_pref_registration_version";
+    public static final String SHARED_PREF_APPVERSION = "shared_pref_appversion_db";
+    public static final String SHARED_PREF_HAS_LEARNED_HELP = "shared_pref_has_learned_help";
+    public static final String SHARED_PREF_IS_FIRST_TIME_LAUNCH = "shared_pref_is_first_time_launch";
+    public static final String SHARED_PREF_INSTITUTION = "shared_pref_institution";
+    public static final String SHARED_PREF_KEY_BOOKINGS_LEFT = "shared_pref_bookings_left";
+    public static final String SHARED_PREF_HASLEARNED_MYACCOUNT = "shared_pref_haslearned_myaccount";
+    public static final String SHARED_PREF_UUID = "shared_pref_uuid" ;
+}
