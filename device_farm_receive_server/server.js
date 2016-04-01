44be8aba06dcbaf4961404c135c3f128ad994777
ObjectiveTruth <j.mendez@rogers.com>
2016-04-01 19:06:39 -0400

    Added logic to server to send apks
    will return 69 if service unavailable

--- device_farm_receive_server/server.js
+++ device_farm_receive_server/server.js
@@ -1,10 +1,19 @@
 const LISTEN_PORT = 9292;
 const ARTIFACTS_SAVE_DIR = '../UoitDCLibraryBooking/UoitDCLibraryBooking/build';
+const DEVICE_FARM_UPLOAD_APKS_FOR_TESTING_ENDPOINT = 
+    'http://api.uoitdclibrarybooking.objectivetruth.ca/circleci_build_webhook/upload_to_devicefarm';
+const NGROK_TUNNEL_URL_ENV_VARIABLE_NAME = 
+    'NGROK_TUNNEL_URL';
+const ANDROID_TEST_INSTRUMENTATION_APK_LOCATION = 
+    'UoitDCLibraryBooking/build/outputs/apk/UoitDCLibraryBooking-debug-androidTest-unaligned.apk';
+const ANDROID_DEBUG_APK_LOCATION = 
+    'UoitDCLibraryBooking/build/outputs/apk/UoitDCLibraryBooking-debug-unaligned.apk';
 
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var multer = require('multer');
+var FormData = require('form-data');
 var storage = multer.diskStorage({
     destination: function (req, file, callback) {
         callback(null, ARTIFACTS_SAVE_DIR);
@@ -15,6 +24,9 @@ var storage = multer.diskStorage({
 });
 var upload = multer({storage: storage});
 
+//==============MAIN=================
+
+sendAPKsToDeviceFarmServer();
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
@@ -24,9 +36,33 @@ app.post('/reply', upload.fields([
 ]), function(req, res) {
         const DEVICE_FARM_RESULT_CODE_FIELDNAME = 'result_code';
         console.log(`Received response from device farm. Result Code: ${res.body[DEVICE_FARM_RESULT_CODE_FIELDNAME]}`);
-    process.exit(res.body[DEVICE_FARM_RESULT_CODE_FIELDNAME]);
+        process.exit(res.body[DEVICE_FARM_RESULT_CODE_FIELDNAME]);
     });
 
 app.listen(LISTEN_PORT, function () {
-    console.log('Device Farm Receive Server listening on port ' + LISTEN_PORT + '!');
+    console.log(`Device Farm Receive Server listening on port ${LISTEN_PORT}!`);
 });
+
+
+
+
+//============Utility Functions================
+
+function sendAPKsToDeviceFarmServer() {
+    console.log('Sending the debug and instrumentation apks to the device farm');
+
+    var requestForCircleCIServer = new FormData();
+    requestForCircleCIServer.append('instrumentation', fs.createReadStream(ANDROID_TEST_INSTRUMENTATION_APK_LOCATION));
+    requestForCircleCIServer.append('debug', fs.createReadStream(ANDROID_DEBUG_APK_LOCATION));
+    requestForCircleCIServer.append('callback', process.env(NGROK_TUNNEL_URL_ENV_VARIABLE_NAME));
+    requestForCircleCIServer.submit(DEVICE_FARM_UPLOAD_APKS_FOR_TESTING_ENDPOINT, function(error, response){
+        if(error) {
+            console.log(`Error sending results to Device Farm Server. Error:  ${error}`);
+            // 69 is Service Unavailable
+            process.exit(69);
+        }else {
+            console.log(`Successfully transferred results to Device Farm Server, will wait for results...`);
+        }
+    });
+    
+}
