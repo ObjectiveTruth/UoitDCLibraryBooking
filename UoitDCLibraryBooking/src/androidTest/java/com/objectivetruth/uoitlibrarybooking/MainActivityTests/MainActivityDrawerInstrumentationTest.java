6921e89fbaafac3af701248b3718cf1a8e3b7d4e
ObjectiveTruth <j.mendez@rogers.com>
2016-04-14 22:08:27 -0400

    Added new tests to confirm the non-working drawer in Main Activity
    Referenced in #10

--- UoitDCLibraryBooking/src/androidTest/java/com/objectivetruth/uoitlibrarybooking/MainActivityTests/MainActivityDrawerInstrumentationTest.java
+++ UoitDCLibraryBooking/src/androidTest/java/com/objectivetruth/uoitlibrarybooking/MainActivityTests/MainActivityDrawerInstrumentationTest.java
@@ -1,9 +1,5 @@
 package com.objectivetruth.uoitlibrarybooking.MainActivityTests;
 
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
-import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.contrib.DrawerActions;
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
@@ -19,14 +15,13 @@ import org.junit.runner.RunWith;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.hamcrest.CoreMatchers.not;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
-public class MainActivityInstrumentationTest {
+public class MainActivityDrawerInstrumentationTest {
     private MainActivity mActivity;
 
     @ClassRule
@@ -44,29 +39,29 @@ public class MainActivityInstrumentationTest {
 
     @After
     public void tearDown() {
-        mActivity = null;
+
     }
 
     @Test
-    public void initialWhatsNewDialogWhenInstalledForFirstTime(){
-        Spoon.screenshot(mActivity, "initial_view");
+    public void isDrawerNotDisplayedAtStart() {
+        _clearWhatsNewDialog();
 
-        onView(withId(android.R.id.button1)).check(matches(isClickable()));
-        Spoon.screenshot(mActivity, "whatsnew_dialog");
-
-        onView(withId(android.R.id.button1)).perform(click());
-        Spoon.screenshot(mActivity, "after_dismiss_whatsnew");
+        Spoon.screenshot(mActivity, "before_opening_drawer");
+        onView(withId(R.id.left_drawer)).check(matches(not((isDisplayed()))));
     }
 
     @Test
-    public void showMyAccountWhenMyIconClicked() {
-        Spoon.screenshot(mActivity, "initial_view");
+    public void isDrawerDisplayedCorrectlyAfterClicking() {
+        _clearWhatsNewDialog();
+
+        Spoon.screenshot(mActivity, "before_opening_drawer");
+
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
+        onView(withId(R.id.left_drawer)).check(matches((isDisplayed())));
+        Spoon.screenshot(mActivity, "after_opening_drawer");
+    }
+
+    private void _clearWhatsNewDialog() {
         onView(withId(android.R.id.button1)).perform(click());
-
-        onView(withId(R.id.user_account)).check(matches(isClickable()));
-        onView(withId(R.id.user_account)).perform(click());
-
-        onView(ViewMatchers.withId(R.id.titleMyAccount)).check(matches(isDisplayed()));
-        Spoon.screenshot(mActivity, "after_my_account_clicked");
     }
 }
