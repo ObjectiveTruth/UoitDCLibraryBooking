7bddc010b6d5ae1e7068b1cae33d2a3b018d67ff
ObjectiveTruth <miguel@objectivetruth.ca>
2016-07-09 14:43:14 -0400

    Reworked the tests to coincide with prev commit

--- UoitDCLibraryBooking/src/test/java/com/objectivetruth/uoitlibrarybooking/data/models/CalendarModelTest.java
+++ UoitDCLibraryBooking/src/test/java/com/objectivetruth/uoitlibrarybooking/data/models/CalendarModelTest.java
@@ -1,22 +1,38 @@
 package com.objectivetruth.uoitlibrarybooking.data.models;
 
-import com.objectivetruth.uoitlibrarybooking.SillyNameValuePairClass;
+import android.content.Context;
+import android.content.SharedPreferences;
+import com.objectivetruth.uoitlibrarybooking.app.UOITLibraryBookingApp;
+import com.objectivetruth.uoitlibrarybooking.data.DataModule;
+import com.objectivetruth.uoitlibrarybooking.data.models.calendarmodel.CalendarWebService;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
-import static org.junit.Assert.assertFalse;
+import static com.objectivetruth.uoitlibrarybooking.common.constants.SHARED_PREFERENCE_NAMES.CALENDAR_SHARED_PREFERENCES_NAME;
+import static org.junit.Assert.assertTrue;
 
 public class CalendarModelTest {
-    private SillyNameValuePairClass mSillyNameValuePairClass;
+    private SharedPreferences sharedPreferencesMock;
+    private SharedPreferences.Editor sharedPreferencesEditorMock;
+    private UOITLibraryBookingApp applicationMock;
+    private CalendarWebService calendarWebServiceMock;
 
     @Before
     public void setUp() throws Exception {
-        mSillyNameValuePairClass = new SillyNameValuePairClass("", "", "", "");
+        sharedPreferencesMock = Mockito.mock(SharedPreferences.class);
+        sharedPreferencesEditorMock = Mockito.mock(SharedPreferences.Editor.class);
+        applicationMock = Mockito.mock(UOITLibraryBookingApp.class);
+        calendarWebServiceMock = Mockito.mock(CalendarWebService.class);
+        DataModule dataModuleMock = Mockito.mock(DataModule.class);
+
+        Mockito.when(sharedPreferencesMock.edit()).thenReturn(sharedPreferencesEditorMock);
+        Mockito.when(applicationMock.getSharedPreferences(CALENDAR_SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE))
+                .thenReturn(sharedPreferencesMock);
     }
 
-
     @Test
-    public void sillyNameValuePair_emptyArguments_isNotEmpty() {
-        assertFalse(mSillyNameValuePairClass.toString().isEmpty());
+    public void calendarModel_emptyArguments_isNotEmpty() {
+        assertTrue(true);
     }
 }
